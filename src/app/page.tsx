11ec0bd08c51fa878838a1d285d083f52041b3ff
Dheeraj Purohit <purohitdheeraj89@gmail.com>
2025-01-18 19:59:48 +0530

    add furniture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import CategoryCommon from "@/components/category-common";
 import FeaturedCategory from "@/components/featured-category";
 import Hero from "@/components/hero";
 import ListProducts from "@/components/list-products";
@@ -65,6 +66,39 @@ const lightProducts: any = [
   },
 ];
 
+const furnitureProducts: any = [
+  {
+    imageSrc: "/assets/furniture/chair.png",
+    imageAlt: "Light 1",
+    title: "Elegant Light Fixture",
+    subtitle: "Classic design with modern touch",
+  },
+  {
+    imageSrc: "/assets/furniture/table-wood.png",
+    imageAlt: "Light 2",
+    title: "Modern Light Fixture",
+    subtitle: "Sleek and stylish",
+  },
+  {
+    imageSrc: "/assets/furniture/table.png",
+    imageAlt: "Light 3",
+    title: "Vintage Light Fixture",
+    subtitle: "Timeless elegance",
+  },
+  {
+    imageSrc: "/assets/furniture/mirror.png",
+    imageAlt: "Light 4",
+    title: "Rustic Light Fixture",
+    subtitle: "Warm and cozy",
+  },
+  {
+    imageSrc: "/assets/furniture/sofa.png",
+    imageAlt: "Light 5",
+    title: "Industrial Light Fixture",
+    subtitle: "Bold and functional",
+  },
+];
+
 
 
 export default function Home() {
@@ -80,6 +114,25 @@ export default function Home() {
       <div className="mt-32">
       <ListProducts col={4} products={chimneyProducts} title={'Our latest chimneypieces'}/>
       <ListProducts col={5} products={lightProducts} title={'Our latest lighting'}/>
+      </div>
+      <div className="px-10">
+      <CategoryCommon
+    title="Furniture"
+    description="Lorem ipsum dolor sit amet, incididunt ut labore et dolore consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim labore et dolore magna ad minim veniam."
+    buttons={[
+      { label: 'Explore our Furniture', variant: 'outline', onClick: () => console.log('Explore clicked') },
+    ]}
+    image={{
+      src: '/assets/furniture-banner.png',
+      alt: 'furniture',
+      width: 584,
+      height: 734,
+    }}
+  />
+      </div>
+
+      <div className="mt-32">
+      <ListProducts col={5} products={furnitureProducts} title={'Our latest furniture'}/>
       </div>
     </>
   );
