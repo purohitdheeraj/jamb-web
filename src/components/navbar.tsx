8c58dc2bfa0b2abc0ca4c94ea50e0af14f133fbf
Dheeraj Purohit <purohitdheeraj89@gmail.com>
2025-01-18 18:45:34 +0530

    fix: nav logo

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,14 @@
 'use client';
 import Image from "next/image";
 import Icon from "./icon";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <nav className="flex items-center justify-between py-8 px-10">
-        <Image src={'/assets/jamb-logo.png'} alt="jamb" width={104} height={104}/>
+        <Link href={'/'}>
+        <Image src={'/assets/jamb-logo.svg'} alt="jamb" width={104} height={104}/>
+        </Link>
 
         <div className="flex items-center space-x-5">
           <Icon name="search" fill={false} color=""/>
